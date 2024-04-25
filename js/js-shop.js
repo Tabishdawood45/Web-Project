574a6a015864efbc145225114e945258ac0e8640
HaCuong04 <tranhacuong090904@gmail.com>
2024-04-25 06:02:45 +0300

    fix overall visual

--- js/js-shop.js
+++ js/js-shop.js
@@ -12,7 +12,7 @@ let shopItemsData = [{
 },
 {
     id: "2",
-    name: "WORKOUT SHIRT",
+    name: "WORKOUT GYM-SHIRT",
     price: 20,
     description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
     img: "images/sport clothes 2.jpg"
@@ -20,14 +20,14 @@ let shopItemsData = [{
 {
     id: "3",
     name: "HEVYWEIGHT HOODY",
-    price: 60,
+    price: 40,
     description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
     img: "images/sport clothes 3.jpg"
 },
 {
     id: "4",
     name: "OUTDOOR SPORT SUIT",
-    price: 70,
+    price: 90,
     description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
     img: "images/sport clothes 4.jpg"
 }]
