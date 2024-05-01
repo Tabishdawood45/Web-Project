842a44d24071a2834c1bc394802693e758617680
Tabishdawood45 <Tabishdawood54@gmail.com>
2024-05-01 14:33:11 +0300

    fix some errors of logo and added some product picture

--- js/class/products.js
+++ js/class/products.js
@@ -3,28 +3,28 @@ let shopItemsData = [
         id: "1",
         name: "SCULPT ZIP THROUGH JACKET",
         price: 35,
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        description: "Sculpt Seamless Ribbed Zip Through Jacket",
         img: "images/sport clothes1.jpg"
     },
     {
         id: "2",
         name: "WORKOUT GYM-SHIRT",
         price: 20,
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        description: "SEAMLESS T-SHIRT FOR MEN & WOMEN",
         img: "images/sport clothes 2.jpg"
     },
     {
         id: "3",
         name: "HEVYWEIGHT HOODY",
         price: 40,
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        description: "Cosy, Comfy And Made For Everyday Wear",
         img: "images/sport clothes 3.jpg"
     },
     {
         id: "4",
         name: "OUTDOOR SPORT SUIT",
         price: 90,
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing.",
+        description: "Waterproof Outdoor Suit For Men & Women",
         img: "images/sport clothes 4.jpg"
     }
 ]
