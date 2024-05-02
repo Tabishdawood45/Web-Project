d676ae26b3baa1c8994578b9655da864daad9539
hduy0407 <t3ngdu01@students.oamk.fi>
2024-05-02 03:46:25 +0300

    update for products, shop at frontend

--- js/class/products.js
+++ js/class/products.js
@@ -1,31 +1,39 @@
-let shopItemsData = [
-    {
-        id: "1",
-        name: "SCULPT ZIP THROUGH JACKET",
-        price: 35,
-        description: "Sculpt Seamless Ribbed Zip Through Jacket",
-        img: "images/sport clothes1.jpg"
-    },
-    {
-        id: "2",
-        name: "WORKOUT GYM-SHIRT",
-        price: 20,
-        description: "SEAMLESS T-SHIRT FOR MEN & WOMEN",
-        img: "images/sport clothes 2.jpg"
-    },
-    {
-        id: "3",
-        name: "HEVYWEIGHT HOODY",
-        price: 40,
-        description: "Cosy, Comfy And Made For Everyday Wear",
-        img: "images/sport clothes 3.jpg"
-    },
-    {
-        id: "4",
-        name: "OUTDOOR SPORT SUIT",
-        price: 90,
-        description: "Waterproof Outdoor Suit For Men & Women",
-        img: "images/sport clothes 4.jpg"
+class Products {
+    #id
+    #name
+    #price
+    #description
+    #image
+
+    constructor (id, message) {
+        this.#id = id
+        this.#name = name
+        this.#price = price
+        this.#description = description
+        this.#image = image
     }
-]
+
+    getId() {
+        return this.#id
+    }
+
+    getName() {
+        return this.#name
+    }
+
+    getPrice() {
+        return this.#price
+    }
+
+    getDescription() {
+        return this.#description
+    }
+
+    getImage() {
+        return this.#image
+    }
+}
+
+export {Products}
+
 
--- js/js-shop.js
+++ js/js-shop.js
@@ -1,55 +1,69 @@
 
-let products = document.getElementById('products')
-
 
 let basket = JSON.parse(localStorage.getItem("data")) || [];
 
-let generateShop = () => {
-    return (products.innerHTML = shopItemsData
-        .map((item) => {
 
-        return `
-        <div id=product-id-${item.id} class="item">
-                <img width="220" height="280" src="${item.img}" alt="">
+const productContainer = document.querySelector('.products-container')
+
+fetch('http://localhost:3001', {
+    method: 'GET',
+    headers: {
+        'Content-Type':'application/json',
+        'Accept': 'application/json'
+    }
+}).then(res => res.json())
+.then(data => {
+    data.forEach(product => {
+        
+        const div = document.createElement('div')
+        div.classList.add('item')
+        div.setAttribute('product-id', product.id)
+        div.innerHTML = 
+        `
+                <img width="220" height="280" src="${product.image}" alt="">
                 <div class="details">
-                    <h4>${item.name}</h4>
-                    <p>${item.description}</p>
+                    <h4>${product.name}</h4>
+                    <p>${product.description}</p>
                     <div class="price">
-                        <h2>€ ${item.price}</h2>
+                        <h2>€ ${product.price}</h2>
                     </div>
-                    <button onclick="increment(${item.id})" class="buy-button"> Add to cart</button>
+                    <button onclick="increment(${product.id})" class="buy-button"> Add to cart</button>
                 </div>
-            </div>
-            `
-    }).join(""));
-};
+        `
+        productContainer.appendChild(div)
+        
+    });
+}) 
+
+
+
 
 let increment = (id) => {
-    let selectedItem = id
-    let search = basket.find((items)=> items.id === id);
-    if(search === undefined) {
+    let selectedItem = id;
+    let search = basket.find((x) => x.id === selectedItem.id);
+    
+    if (search === undefined) {
         basket.push({
         id: selectedItem.id,
-        item: 1
-    })
+        item: 1,
+        });
     } else {
-        search.item += 1
+        search.item += 1;
     }
     
-    update(selectedItem.id)
-    localStorage.setItem("data", JSON.stringify(basket))
-}
+    update(selectedItem.id);
+    localStorage.setItem("data", JSON.stringify(basket));
+};
+
 
 let update = (id) => {
-    let search = basket.find((items)=> items.id === id)
-    console.log(search.item)
-    add()
-}
+    let search = basket.find((x) => x.id === id);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+};
 
-let add = () => {
+let calculation = () => {
     let cartIcon = document.getElementById("cartAmount");
-    cartIcon.innerHTML = basket.map((items)=> items.item).reduce((x, y) => x + y, 0)
-}    
+    cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+  };
 
-generateShop()
-add()
